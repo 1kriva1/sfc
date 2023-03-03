c9777aedc8f1e8c71b1f566d78538811d985f78a
Andrii Kryvoruk <AndriiK@neogames.com>
2023-03-03 12:08:46 +0200

    not found page done

---
+++ src/app/core/pages/not-found/directives/ball/canvas-ball.constants.ts
@@ -0,0 +1,4 @@
+export class CanvasBallConstants {
+    static BALL_IMAGE_SRC = "/assets/images/core/pages/not-found/ball.png";
+    static BALL_ANIMATION_INTERVAL = 33;
+}
--- src/app/core/pages/not-found/not-found.page.component.ts
+++ src/app/core/pages/not-found/not-found.page.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, ChangeDetectorRef, Component, Inject } from '@angular/core';
+import { ButtonType, MediaLimits, WINDOW } from 'ngx-sfc-common';
+import { NotFoundPageConstants } from './not-found.page.constants';
 
 @Component({
   templateUrl: './not-found.page.component.html',
   styleUrls: ['./not-found.page.component.scss']
 })
-export class NotFoundPageComponent {
+export class NotFoundPageComponent implements AfterViewChecked {
 
+  ButtonType = ButtonType;
+
+  public get size(): number {
+    return NotFoundPageConstants.DEFAULT_SIZE * this.sizeFactor;
+  }
+
+  private get sizeFactor() {
+    if (this.window.outerWidth <= MediaLimits.Tablet)
+      return NotFoundPageConstants.TABLET_AND_LESS_SIZE_FACTOR * this.window.outerWidth / MediaLimits.LaptopLarge;
+    else
+      return NotFoundPageConstants.DEFAULT_SIZE_FACTOR * this.window.outerWidth / MediaLimits.LaptopLarge;
+  }
+
+  constructor(private changeDetector: ChangeDetectorRef, @Inject(WINDOW) private window: Window) { }
+
+  ngAfterViewChecked(): void {
+    this.changeDetector.detectChanges();
+  }
 }
